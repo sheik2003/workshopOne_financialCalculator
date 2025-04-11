db6bad9535e466449fb635f3aa5076b4f9d998e6
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-04-11 15:10:29 -0400

    created most of calculator 1 debugging calculation errors

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -14,14 +14,33 @@ public class Main {
         int givenCommand = scanner.nextInt();
 
 
+        float result = 0;
+        switch (givenCommand) {
+            case 1:
+                System.out.println("--You have selected Financial Calculator 1: This will help you calculate the monthly payment for what your load would be--");
 
+                System.out.println("Please enter the load amount: ");
+                float principal = scanner.nextFloat();
 
+                System.out.println("Please enter the interest rate amount(format e.g., 7.625% = 0.07625): ");
+                float annualInterestRate = scanner.nextFloat();
 
+                System.out.println("Please enter the length of the Loan(years): ");
+                float years = scanner.nextFloat();
 
-//    float result = 0;
-//        switch (givenCommand){
-//        case 1:
-//            break;
+                double monthlyPayment = 0;
+                float totalInterest = 0;
+                int numberOfMonths = 12;
+                float numberOfMonthlyPayments = 12 * years;
+                float monthlyInterestRate = annualInterestRate / 12;
+
+                //calculations
+                monthlyPayment = principal * Math.pow(((monthlyInterestRate*(1 + monthlyInterestRate))),numberOfMonthlyPayments) / (Math.pow((1+monthlyInterestRate),numberOfMonthlyPayments)-1);
+                totalInterest = (monthlyInterestRate * numberOfMonthlyPayments) - principal;
+
+                System.out.println("Your monthly Payment is: " + monthlyPayment);
+                System.out.println("Your total interest paid would be: " + totalInterest);
+                break;
 //        case 2:
 //            result = firstNum - secondNum;
 //            break;
@@ -39,9 +58,7 @@ public class Main {
 //            System.out.println("INVALID COMMAND try again");
 
 
-
+        }
 
     }
-
-
 }
