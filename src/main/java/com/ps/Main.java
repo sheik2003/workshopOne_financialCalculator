d04c2e9b195d267bdfc6bcc75c769d45f319a920
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-04-11 14:20:04 -0400

    imported required libraries and also started taking user input

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,47 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("-----Financial Calculator-----");
+        System.out.println("1) Calculator1");
+        System.out.println("2) Calculator2");
+        System.out.println("3) Calculator3");
+        System.out.print("Please select an operation: ");
+        int givenCommand = scanner.nextInt();
+
+
+
+
+
+
+//    float result = 0;
+//        switch (givenCommand){
+//        case 1:
+//            break;
+//        case 2:
+//            result = firstNum - secondNum;
+//            break;
+//        case 3:
+//            result = firstNum * secondNum;
+//            break;
+//        case 4:
+//            if(secondNum == 0){
+//                System.out.println("You cannot divide by zero");
+//                result = 0;
+//            }else{
+//                result = firstNum / secondNum;}
+//            break;
+//        default:
+//            System.out.println("INVALID COMMAND try again");
+
+
+
+
     }
+
+
 }
