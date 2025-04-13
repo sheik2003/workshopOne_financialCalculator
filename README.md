8f58bb7826292706a834347d5e0c0f904297160c
Sheik siphat <notsheiksiphat2003@gmail.com>
2025-04-13 17:21:00 -0400

    Made the calculators look better

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Financial Calculator
+
+
+---
+
+## 🧮 Calculator Breakdown
+**********
+### ️**📲Home screen**
+
+> The user has the ability to choose which calculator they wants to choose corresponding to the 
+> number of the calculator
+
+
+
+![img.png](img.png)
+
+
+******
+
+
+### 1️⃣ **Mortgage Payment Calculator**
+
--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -6,18 +6,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
 
-        System.out.println("-----Financial Calculator-----");
-        System.out.println("1) Calculator1");
-        System.out.println("2) Calculator2");
-        System.out.println("3) Calculator3");
-        System.out.print("Please select an operation: ");
+        System.out.println("-----\uD83D\uDCB0Financial Calculator\uD83D\uDCB0-----");
+        System.out.println("\uD83D\uDCC9 Calculator 1 - Loan Payment");
+        System.out.println("\uD83D\uDCC8 Calculator 2 - Future Value");
+        System.out.println("\uD83E\uDDFE Calculator 3 - Annuity Value");
+        System.out.println("\uD83D\uDCB9 Calculator 4 - S&P 500 Growth");
+        System.out.print("Please select a Calculator (enter either 1,2,3,4 to choose the calculator): ");
         int givenCommand = scanner.nextInt();
 
 
-      
+
         switch (givenCommand) {
             case 1:
-                System.out.println("--You have selected Financial Calculator 1: This will help you calculate the monthly payment for what your load would be--");
+                System.out.println("--You have selected Financial Calculator 1\uD83D\uDCC9 : This will help you calculate the monthly payment for what your load would be--");
 
                 System.out.println("Please enter the loan amount: ");
                 double principal = scanner.nextDouble();
@@ -41,7 +42,7 @@ public class Main {
                 System.out.println("Your total interest paid would be: " + totalInterest);
                 break;
         case 2:
-            System.out.println("--You have selected Financial Calculator 2: This will help you calculate the future value of a one-time deposit assuming compound interest--");
+            System.out.println("--You have selected Financial Calculator 2\uD83D\uDCC9 : This will help you calculate the future value of a one-time deposit assuming compound interest--");
             System.out.println("Please enter the deposit amount: ");
             double principalC2 = scanner.nextDouble();
 
@@ -61,7 +62,7 @@ public class Main {
             System.out.println("Total interest earned is: " + totalInterestC2);
             break;
         case 3:
-            System.out.println("--You have selected Financial Calculator 3: This will help you determines the present value of an ordinary annuity--");
+            System.out.println("--You have selected Financial Calculator 3\uD83E\uDDFE: This will help you determines the present value of an ordinary annuity--");
             System.out.println("Please enter the monthly payout amount: ");
             double monthlyPayoutAmount = scanner.nextDouble();
 
@@ -79,7 +80,26 @@ public class Main {
 
             break;
 
+            case 4:
+                System.out.println("--You have selected Financial Calculator 4\uD83D\uDCB9: This will help you determines how much your investment in the S&P500 would be if you invested in it a certain year (not adjusted for inflation and assumes average return of 10%)--");
+                System.out.println("Please enter the your investment amount: ");
+                double investedAmount = scanner.nextDouble();
 
+                System.out.println("Please enter the year in which you made the investment: ");
+                double yearInvested = scanner.nextDouble();
+
+                System.out.println("Please enter the year you want to check the investments value for: ");
+                double yearInvestmentCheck = scanner.nextDouble();
+
+                double timeInvested = yearInvestmentCheck - yearInvested;
+                double futureValueOfInvestment;
+
+                futureValueOfInvestment = investedAmount * Math.pow(1+0.1,timeInvested);
+                double profit = futureValueOfInvestment - investedAmount;
+
+                System.out.println("If you've invested " + investedAmount + " in the year " + (int)yearInvested + " You would have " + futureValueOfInvestment + " by the year " + (int) yearInvestmentCheck + " and the total Amount you would've profitted would be: " + profit);
+
+                break;
         }
 
     }
